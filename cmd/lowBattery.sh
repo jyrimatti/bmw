6ea9adf8d9c49024f1985c7eaa32aee09acac0b4
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-07-13 18:48:04 +0300

    whoops

--- cmd/lowBattery.sh
+++ cmd/lowBattery.sh
@@ -2,4 +2,4 @@
 #! nix-shell -i dash --pure --keep CREDENTIALS_DIRECTORY -I channel:nixos-23.05-small -p dash jq python3Packages.bimmer-connected python3Packages.setuptools
 set -eu
 
-test "$(./cmd/batteryPercentage.sh $*)" -lt 50 && echo 1 || echo 0
+test "$(dash ./cmd/batteryPercentage.sh $*)" -lt 50 && echo 1 || echo 0
