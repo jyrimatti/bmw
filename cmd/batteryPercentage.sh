d564aa5336fd6980ff571dea319e4a8aa58b25e5
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-10-15 21:01:43 +0300

    try caching

--- cmd/batteryPercentage.sh
+++ cmd/batteryPercentage.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i dash --pure --keep CREDENTIALS_DIRECTORY -I channel:nixos-23.05-small -p dash jq flock python3Packages.bimmer-connected python3Packages.setuptools
+#! nix-shell -i dash --pure --keep CREDENTIALS_DIRECTORY --keep XDG_RUNTIME_DIR -I channel:nixos-23.05-small -p dash jq flock bkt python3Packages.bimmer-connected python3Packages.setuptools
 set -eu
 
 . ./bmw_env.sh
 BMW_ACCESS_TOKEN="$(dash ./bmw_login.sh)"
 export BMW_ACCESS_TOKEN
 
-python3 ./data.sh fuel_and_battery | jq -r '.remaining_battery_percent'
+bkt --cwd --ttl "1m" --stale "30s" --cache-dir "${XDG_RUNTIME_DIR:-/tmp}/bmw" python3 ./data.sh fuel_and_battery | jq -r '.remaining_battery_percent'
